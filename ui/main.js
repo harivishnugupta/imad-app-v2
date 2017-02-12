e2b9e9c2e97e35d1e8d7581e388cf2590e7b2aa6
harivishnugupta <gupta.vishnu55@gmail.com>
2017-02-12 12:47:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var change= document.getElementById('details');
 change.innerHTML = 'changed value';
 var moving = document.getElementById('toypic');
-movingright()
+var movingright= function()
 {
     marginLeft = marginLeft = 10;
     movingright.style.marginLeft = marginLeft + 'px';
