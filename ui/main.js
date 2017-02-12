4fb6681dc28626e5f7ffb371ca8dce5232811fee
harivishnugupta <gupta.vishnu55@gmail.com>
2017-02-12 12:36:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var change= document.getElementById('details');
 change.innerHTML = 'changed value';
+var moving = document.getElementById('toypic');
+moving.onclick = function()
+{
+    leftmargin = leftmargin + 5;
+    leftmargin = leftmargin + px;
+};
